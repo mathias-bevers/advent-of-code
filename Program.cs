3c72cfdec78ec61b0c6c16820e43328aba93b378
Mathias Bevers <mathiasbevers@outlook.com>
2022-12-05 21:02:20 +0100

    moved 'Input' directory, rewrite of 'Project.cs', updated 'README.md'

--- Program.cs
+++ Program.cs
@@ -5,68 +5,71 @@ using Debug = AdventOfCode.Tools.Debug;
 
 public class Program
 {
-	public static void Main(string[] args)
-	{
-		string year = "2022";
-		if (args.Length > 0) { year = args[0]; }
-		
+    public static void Main(string[] args)
+    {
+        string year = "2022";
+        if (args.Length > 0) { year = args[0]; }
 
-		List<Day> unstortedDays = new();
-		List<Day> days;
+        List<Day> days = new();
 
-		if (year == "all")
-		{
-			foreach (Type type in Assembly.GetExecutingAssembly()
-				         .GetTypes()
-				         .Where(t => t.IsSubclassOf(typeof(Day))))
-			{
-				Day day = (Day)Activator.CreateInstance(type);
-				unstortedDays.Add(day);
-			}
-		}
+        if (year == "all")
+        {
+            foreach (Type type in Assembly.GetExecutingAssembly()
+                         .GetTypes()
+                         .Where(t => t.IsSubclassOf(typeof(Day))))
+            {
+                Day day = (Day)Activator.CreateInstance(type);
+                days.Add(day);
+            }
+        }
+        else
+        {
+            foreach (Type type in Assembly.GetExecutingAssembly()
+                         .GetTypes()
+                         .Where(t => t.Namespace == $"AdventOfCode._{year}" && t.IsSubclassOf(typeof(Day))))
+            {
+                Day day = (Day)Activator.CreateInstance(type);
+                days.Add(day);
+            }
+        }
+
+        days.Sort();
+
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        Console.WriteLine("Advent Of Code by Mathias Bevers");
+        Console.ResetColor();
+
+        if (days.Count >= 1)
+        {
+            Stopwatch stopwatch = new();
+            foreach (Day day in days)
+            {
+                double elapsedTime = day.Initialize();
+                Console.WriteLine($"\nInitialized day: {day.Year}-{day.DayNumber} in {elapsedTime}ms");
+
+                try
+                {
+                    stopwatch.Restart();
+                    string answer = day.StarOne();
+                    Day.Answer(day.DayNumber, 1, answer, stopwatch.ElapsedMilliseconds);
+                }
+                catch (NotImplementedException) { Debug.LogWaring("Star one is not implemented."); }
+
+                try
+                {
+                    stopwatch.Restart();
+                    string answer = day.StarTwo();
+                    Day.Answer(day.DayNumber, 2, answer, stopwatch.ElapsedMilliseconds);
+                }
+                catch (NotImplementedException) { Debug.LogWaring("Star two is not implemented."); }
+            }
+        }
 		else
 		{
-			foreach (Type type in Assembly.GetExecutingAssembly()
-				         .GetTypes()
-				         .Where(t => t.Namespace == $"AdventOfCode._{year}" && t.IsSubclassOf(typeof(Day))))
-			{
-				Day day = (Day)Activator.CreateInstance(type);
-				unstortedDays.Add(day);
-			}
+			Debug.LogWaring($"There are no days made in the year: {year}");
 		}
 
-		unstortedDays.Sort();
-		days = unstortedDays;
-
-
-		Console.ForegroundColor = ConsoleColor.DarkCyan;
-		Console.WriteLine("Advent Of Code by Mathias Bevers");
-		Console.ResetColor();
-
-		Stopwatch stopwatch	= new();
-		foreach (Day day in days)
-		{
-			double elapsedTime = day.Initialize();
-			Console.WriteLine($"\nInitialized day: {day.Year}-{day.DayNumber} in {elapsedTime}ms");
-
-			try
-			{
-				stopwatch.Restart();
-				string answer = day.StarOne();
-				Day.Answer(day.DayNumber, 1, answer, stopwatch.ElapsedMilliseconds);
-			}
-			catch (NotImplementedException) { Debug.LogWaring("Star one is not implemented."); }
-
-			try
-			{
-				stopwatch.Restart();
-				string answer = day.StarTwo();
-				Day.Answer(day.DayNumber, 2, answer, stopwatch.ElapsedMilliseconds);
-			}
-			catch (NotImplementedException) { Debug.LogWaring("Star two is not implemented."); }
-		}
-
-		Console.Write("Press enter to close...");
-		Console.ReadLine();
-	}
+        Console.Write("Press enter to close...");
+        Console.ReadLine();
+    }
 }
--- Tools/DataRetriever.cs
+++ Tools/DataRetriever.cs
@@ -10,11 +10,29 @@
 
 		private static string GeneratePath(Day day)
 		{
-			string path = $@"{Directory.GetCurrentDirectory()}\Input\{day.Year}\{day.GetType().Name}.txt";
+			string solutionPath = TryFindSolutionPath();
+			string path = solutionPath + $@"\Input\{day.Year}\{day.GetType().Name}.txt";
 
 			if (!File.Exists(path)) { Debug.LogError($"There is no file for day {day.Year}-{day.DayNumber}"); }
 
 			return path;
+		}
+
+		private static string TryFindSolutionPath(string currentPath = "")
+		{
+			DirectoryInfo directory = new DirectoryInfo(string.IsNullOrEmpty(currentPath) ? Directory.GetCurrentDirectory() : currentPath);
+			
+			while (directory != null)
+			{
+				if (directory.GetFiles("*.sln").Any())
+				{
+					return directory.FullName;
+				}
+
+				directory = directory.Parent;
+			}
+
+			throw new DirectoryNotFoundException("Could not find .sln");
 		}
 	}
 }
