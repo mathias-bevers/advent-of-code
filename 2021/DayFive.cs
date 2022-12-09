bd11847c9a9f963fb3c31986223656a8f6f983ed
Mathias Bevers <mathiasbevers@outlook.com>
2022-12-09 08:48:08 +0100

    2021 day 4 part one and start part 2

---
+++ 2021/DayFive.cs
@@ -0,0 +1,149 @@
+﻿using AdventOfCode.Tools;
+
+namespace AdventOfCode._2021
+{
+	public class DayFive : Day
+	{
+		public override int DayNumber => 5;
+
+		//private readonly int[,] grid = new int[991, 989];
+		private readonly int[,] grid = new int[10, 10];
+		private Line[] lines;
+
+		public override double Initialize()
+		{
+			stopwatch.Start();
+
+			List<Line> lineList = new();
+
+			foreach (string asLine in DataRetriever.AsLines(this))
+			{
+				string[] lines = asLine.Split(" -> ");
+				string[] start = lines[0].Split(',');
+				string[] end = lines[1].Split(',');
+
+				Vector2Int startPoint = new (int.Parse(start[0]), int.Parse(start[1]));
+				Vector2Int endPoint = new(int.Parse(end[0]), int.Parse(end[1]));
+				float differenceAngle = (float)((startPoint - endPoint).angle * 180 / Math.PI);
+				Debug.Log(differenceAngle);
+
+				if (startPoint.x == endPoint.x || startPoint.y == endPoint.y)
+				{
+					lineList.Add(new Line(new Vector2Int(startPoint.x, startPoint.y), new Vector2Int(endPoint.x, endPoint.y), false));
+				}
+				else if (differenceAngle == 45.0f)
+				{
+					lineList.Add(new Line(new Vector2Int(startPoint.x, startPoint.y), new Vector2Int(endPoint.x, endPoint.y), true));
+				}
+			}
+
+			lines = lineList.ToArray();
+
+			stopwatch.Stop();
+			return stopwatch.ElapsedMilliseconds;
+		}
+
+		public override string StarOne()
+		{
+			foreach (Line line in lines)
+			{
+				switch (line.Dir)
+				{
+					case Line.Direction.Horizontal:
+					{
+						for (int x = line.Start.x; x <= line.End.x; ++x) { ++grid[x, line.Start.y]; }
+
+						break;
+					}
+					case Line.Direction.Vertical:
+					{
+						for (int y = line.Start.y; y <= line.End.y; ++y) { ++grid[line.Start.x, y]; }
+
+						break;
+					}
+					case Line.Direction.Diagonal: continue;
+					default: throw new ArgumentOutOfRangeException();
+				}
+			}
+
+			return grid.Cast<int>().Count(i => i >= 2).ToString();
+		}
+
+		public override string StarTwo()
+		{
+			 int[,] grid = new int[10, 10];
+
+			foreach (Line line in lines.Where(l => l.Dir == Line.Direction.Diagonal))
+			{
+				Debug.Log(line);
+
+				for (int y = line.Start.y; y <= line.End.y; ++y)
+				for (int x = line.Start.x; x <= line.End.x; ++x)
+				{
+					if (y != x) { continue; }
+
+					++grid[x, y];
+				}
+			}
+
+			for (int y = 0; y < grid.GetLength(0); ++y)
+			{
+				for (int x = 0; x < grid.GetLength(0); ++x)
+				{
+					Console.Write(grid[x, y]);
+				}
+
+				Console.WriteLine();
+			}
+			
+
+			return grid.Cast<int>().Count(i => i >= 2).ToString();
+		}
+
+		private struct Line
+		{
+			public enum Direction
+			{
+				Horizontal,
+				Vertical,
+				Diagonal
+			}
+
+			public Direction Dir { get; }
+			public Vector2Int End { get; }
+			public Vector2Int Start { get; }
+
+			public Line(Vector2Int start, Vector2Int end, bool isDiagonal)
+			{
+				if (isDiagonal)
+				{
+					Dir = Direction.Diagonal;
+
+					Start = start;
+					End = end;
+					return;
+				}
+
+				Dir = start.y == end.y ? Direction.Horizontal : Direction.Vertical;
+
+				if (start.x <= end.x && start.y <= end.y)
+				{
+					Start = start;
+					End = end;
+				}
+				else
+				{
+					Start = end;
+					End = start;
+				}
+			}
+
+			public override string ToString()
+			{
+				string[] parts = { $"Line from {Start,-15}", $"to {End,-15}", $"is {Dir}" };
+
+				return string.Concat(parts);
+			}
+		}
+	}
+}
--- Tools/Vector2.cs
+++ Tools/Vector2.cs
@@ -2,8 +2,26 @@
 {
 	public struct Vector2
 	{
+		public float magnitute
+		{
+			get => (float)Math.Sqrt((x * x) + (y * y));
+			set => this *= value;
+		}
+
 		public float x { get; set; }
 		public float y { get; set; }
+
+		public int angle
+		{
+			get => (int)Math.Atan2(y, x);
+			set
+			{
+				float m = magnitute;
+				x = (int)Math.Round(Math.Cos(value) * m);
+				y = (int)Math.Round(Math.Sin(value) * m);
+			}
+		}
+
 
 		public Vector2(float x, float y)
 		{
@@ -14,10 +32,39 @@
 		public Vector2(float value) => x = y = value;
 
 		public Vector2() => x = y = 0;
+
+		public static Vector2 operator +(Vector2 a, Vector2 b) => new(a.x + b.x, a.y + b.y);
+		public static Vector2 operator -(Vector2 a, Vector2 b) => new(a.x - b.x, a.y - b.y);
+		public static Vector2 operator *(Vector2 a, float scale) => new(a.x * scale, a.y * scale);
+		public static Vector2 operator *(float scale, Vector2 a) => new(a.x * scale, a.y * scale);
+		public static Vector2 operator /(Vector2 a, float scale) => new(a.x / scale, a.y * scale);
+		public static bool operator ==(Vector2 a, Vector2 b) => a.x == b.x && a.y == b.y;
+		public static bool operator !=(Vector2 a, Vector2 b) => a.x != b.x || a.y != b.y;
+
+		public static implicit operator Vector2Int(Vector2 v) => new((int)Math.Round(v.x), (int)Math.Round(v.y));
+
+		public override string ToString() => $"({x}, {y})";
 	}
 
 	public struct Vector2Int
 	{
+		public float angle
+		{
+			get => (float)Math.Atan2(y, x);
+			set
+			{
+				float m = magnitute;
+				x = (int)Math.Round(Math.Cos(value) * m);
+				y = (int)Math.Round(Math.Sin(value) * m);
+			}
+		}
+
+		public float magnitute
+		{
+			get => (float)Math.Sqrt((x * x) + (y * y));
+			set => this *= value;
+		}
+
 		public int x { get; set; }
 		public int y { get; set; }
 
@@ -30,5 +77,18 @@
 		public Vector2Int(int value) => x = y = value;
 
 		public Vector2Int() => x = y = 0;
+
+
+		public static Vector2Int operator +(Vector2Int a, Vector2Int b) => new(a.x + b.x, a.y + b.y);
+		public static Vector2Int operator -(Vector2Int a, Vector2Int b) => new(a.x - b.x, a.y - b.y);
+		public static Vector2Int operator *(Vector2Int v, float scale) => new((int)(v.x * scale), (int)(v.y * scale));
+		public static Vector2Int operator *(float scale, Vector2Int v) => new((int)(v.x * scale), (int)(v.y * scale));
+		public static Vector2Int operator /(Vector2Int a, float scale) => new((int)(a.x / scale), (int)(a.y / scale));
+		public static bool operator ==(Vector2Int a, Vector2Int b) => a.x == b.x && a.y == b.y;
+		public static bool operator !=(Vector2Int a, Vector2Int b) => a.x != b.x || a.y != b.y;
+
+		public static implicit operator Vector2(Vector2Int v) => new(v.x, v.y);
+
+		public override string ToString() => $"({x}, {y})";
 	}
 }
