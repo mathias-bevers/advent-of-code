dc4652b7c420fecc8c8ffad4831ed61a6ccd78d5
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-01 20:19:56 +0100

    solved star one
    - added the solution for star one which caculates the difference between the left en right number in a sorted list.

--- src/days/2024/Day0124.cs
+++ src/days/2024/Day0124.cs
@@ -6,7 +6,7 @@ namespace advent_of_code.days;
 internal class Day0124 : IDay
 {
     public DateTime date { get; } = new DateTime(2024, 12, 01);
-    
+
     private int[][] data = new int[2][];
 
     public void PopulateData(string raw)
@@ -15,11 +15,11 @@ internal class Day0124 : IDay
         data[0] = new int[pairLines.Length];
         data[1] = new int[pairLines.Length];
 
-        for(int i = 0; i < pairLines.Length; ++i)
+        for (int i = 0; i < pairLines.Length; ++i)
         {
             string[] numbers = pairLines[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
             data[0][i] = int.Parse(numbers[0]);
-            data[1][i] = int.Parse(numbers[1]);       
+            data[1][i] = int.Parse(numbers[1]);
         }
 
         Array.Sort(data[0]);
@@ -28,7 +28,14 @@ internal class Day0124 : IDay
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        int difference = 0;
+
+        for (int i = 0; i < data[0].Length; ++i)
+        {
+            difference += Math.Abs(data[0][i] - data[1][i]);
+        }
+
+        return difference.ToString();
     }
 
     public string SolveStarTwo()
