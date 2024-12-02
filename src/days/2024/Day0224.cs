3efa2ed365cc2b7304f6d9da2888a62a9274f9c7
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-02 10:30:58 +0100

    rewritten the is-save-difference checking and attempt one star two failed
    - rewritten the is-save-difference check to a function since it comes
    back in the second star which would lead to a lot of duplicate code.
    - the first attempt at star two resulted in '695' which is too low.

--- src/days/2024/Day0224.cs
+++ src/days/2024/Day0224.cs
@@ -34,36 +34,10 @@ internal class Day0224 : IDay
             bool? isIncline = null;
             for (int ii = 0; ii < data[i].Length - 1; ++ii)
             {
-                int difference = data[i][ii + 1] - data[i][ii];
-                int absoluteDifference = Math.Abs(difference);
-
-                if (absoluteDifference < 1 || absoluteDifference > 3)
+                if (!IsSafeDifference(data[i][ii + 1], data[i][ii], ref isIncline))
                 {
                     isSave = false;
                     break;
-                }
-
-                if (!isIncline.HasValue)
-                {
-                    isIncline = difference > 0;
-                    continue;
-                }
-
-                if (isIncline.Value)
-                {
-                    if (difference < 0)
-                    {
-                        isSave = false;
-                        break;
-                    }
-                }
-                else
-                {
-                    if (difference > 0)
-                    {
-                        isSave = false;
-                        break;
-                    }
                 }
             }
 
@@ -76,6 +50,83 @@ internal class Day0224 : IDay
 
     public string SolveStarTwo()
     {
-        throw new NotImplementedException();
+        int saveLevels = 0;
+        for (int i = 0; i < data.Length; ++i)
+        {
+            bool isSave = true;
+            bool? isIncline = null;
+
+            for (int ii = 0; ii < data[i].Length - 1; ++ii)
+            {
+                if (!IsSafeDifference(data[i][ii + 1], data[i][ii], ref isIncline))
+                {
+                    isSave = false;
+                    break;
+                }
+            }
+
+            if (isSave)
+            {
+                ++saveLevels;
+                continue;
+            }
+
+
+            for (int ii = 1; ii < data[i].Length - 1; ++ii)
+            {
+                isSave = true;
+                for (int iii = 0; iii < data[i].Length - 1; ++iii)
+                {
+                    int right = ii == iii ? iii - 1 : iii;
+                    if (!IsSafeDifference(data[i][iii + 1], data[i][right], ref isIncline))
+                    {
+                        isSave = false;
+                        break;
+                    }
+                }
+
+                if (isSave) { break; }
+            }
+
+            if (!isSave) { continue; }
+
+            ++saveLevels;
+        }
+
+        return saveLevels.ToString();
+    }
+
+    private bool IsSafeDifference(int left, int right, ref bool? isIncline)
+    {
+        int difference = right - left;
+        int absoluteDifference = Math.Abs(difference);
+
+        if (absoluteDifference < 1 || absoluteDifference > 3)
+        {
+            return false;
+        }
+
+        if (!isIncline.HasValue)
+        {
+            isIncline = difference > 0;
+            return true;
+        }
+
+        if (isIncline.Value)
+        {
+            if (difference < 0)
+            {
+                return false;
+            }
+        }
+        else
+        {
+            if (difference > 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
