f95dadc5007f3f3a280c08a43e368e9438682efa
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-03 12:34:18 +0100

    solved star two
    - made the multiplication array to an instruction tuple array, might be
    string array if they are ordered by default.
    - added the 'don't()' and 'do()' function to the regex string.
    - enabled the count on the do and don't functions.
    - only added the multiplication if the inscructions are enabled.

--- src/days/2024/Day0324.cs
+++ src/days/2024/Day0324.cs
@@ -6,31 +6,34 @@ internal class Day0324 : IDay
 {
     public DateTime date { get; } = new(2024, 12, 03);
 
-    string[] multiplications = [];
+    (int index, string instruction)[] instructionPairs = [];
 
     public void PopulateData(string raw)
     {
-        MatchCollection maches = Regex.Matches(raw,  @"mul\([0-9]{1,3},[0-9]{1,3}\)");
+        MatchCollection maches =
+            Regex.Matches(raw, @"(mul\([0-9]{1,3},[0-9]{1,3}\))|(don't\(\))|(do\(\))");
 
-        multiplications = new string[maches.Count];
-        for (int i = 0; i < multiplications.Length; ++i)
+        instructionPairs = new (int, string)[maches.Count];
+        for (int i = 0; i < instructionPairs.Length; ++i)
         {
-            multiplications[i] = maches[i].Value;
+            Match match = maches[i];
+            instructionPairs[i] = (match.Index, match.Value);
         }
+
+        Array.Sort(instructionPairs, (a, b) => a.index.CompareTo(b.index));
     }
 
     public string SolveStarOne()
     {
         int instructionSum = 0;
 
-        for(int i = 0; i < multiplications.Length; ++i)
+        for (int i = 0; i < instructionPairs.Length; ++i)
         {
-            string multiplication = multiplications[i];
-            multiplication = multiplication[4..];
-            multiplication = multiplication[..^1];
+            string instruction = instructionPairs[i].instruction;
 
-            string[] digits = multiplication.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            instructionSum += int.Parse(digits[0]) * int.Parse(digits[1]);
+            if (!instruction.StartsWith("mul(")) { continue; }
+
+            instructionSum += ParseMuliplicationInstruction(instruction);
         }
 
         return instructionSum.ToString();
@@ -38,6 +41,36 @@ internal class Day0324 : IDay
 
     public string SolveStarTwo()
     {
-        throw new NotImplementedException();
+        int instructionSum = 0;
+        bool isEnabled = true;
+
+        for (int i = 0; i < instructionPairs.Length; ++i)
+        {
+            string instruction = instructionPairs[i].instruction;
+
+            if (instruction.StartsWith("mul("))
+            {
+                if (!isEnabled) { continue; }
+                instructionSum += ParseMuliplicationInstruction(instruction);
+            }
+            else if (instruction.Equals("don't()")) { isEnabled = false; }
+            else if (instruction.Equals("do()")) { isEnabled = true; }
+            else
+            {
+                throw new InvalidOperationException("cannot parse the instruction: "
+                    + instruction);
+            }
+        }
+
+        return instructionSum.ToString();
+    }
+
+    private int ParseMuliplicationInstruction(string input)
+    {
+        input = input[4..];     // remove 'mul('.
+        input = input[..^1];    // remove ')'.
+
+        string[] digits = input.Split(',', StringSplitOptions.RemoveEmptyEntries);
+        return int.Parse(digits[0]) * int.Parse(digits[1]);
     }
 }
