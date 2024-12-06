d39c5a11b21bde360f3b428227f6e7d1b838ff79
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-06 14:16:12 +0100

    attempt one for star two
    - the value 928 is too low.

--- src/days/2024/Day0624.cs
+++ src/days/2024/Day0624.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using advent_of_code.utils;
 
@@ -12,8 +13,7 @@ internal class Day0624 : IDay
     private const char OBSTACLE = '#';
 
     private char[,] map = new char[0, 0];
-    private Vector2Int position = new();
-    private Vector2Int direction = new(0, 1);
+    private Vector2Int origin = new();
 
     public void PopulateData(string raw)
     {
@@ -31,8 +31,8 @@ internal class Day0624 : IDay
 
                 if (current != START_POINT) { continue; }
 
-                position.x = x;
-                position.y = y;
+                origin.x = x;
+                origin.y = y;
             }
         }
     }
@@ -44,17 +44,27 @@ internal class Day0624 : IDay
         int limit = width * height * 10;
         int n = 0;
 
+        Vector2Int position = origin;
+        Vector2Int direction = new(0, 1);
+
+
+        if (map.Clone() is not char[,] copy)
+        {
+            Logger.Error("something went wrong during copying the map");
+            return "error";
+        }
+
         do
         {
             ++n;
 
-            map[position.x, position.y] = VISITED_POINT;
+            copy[position.x, position.y] = VISITED_POINT;
 
             Vector2Int next = position + new Vector2Int(direction.x, -direction.y);
 
-            if (next.x < 0 || next.x >= width || next.y < 0 || next.y >= height) { break; }
+            if (!IsPointInGrid(next)) { break; }
 
-            if (map[next.x, next.y] != OBSTACLE)
+            if (copy[next.x, next.y] != OBSTACLE)
             {
                 position = next;
                 continue;
@@ -70,7 +80,7 @@ internal class Day0624 : IDay
         {
             for (int x = 0; x < width; ++x)
             {
-                if (map[x, y] != VISITED_POINT) { continue; }
+                if (copy[x, y] != VISITED_POINT) { continue; }
 
                 ++visited;
             }
@@ -81,6 +91,70 @@ internal class Day0624 : IDay
 
     public string SolveStarTwo()
     {
-        throw new NotImplementedException();
+        int infinites = 0;
+
+        for (int y = origin.y; y < map.GetLength(1); ++y)
+        {
+            for (int x = 0; x < map.GetLength(0); ++x)
+            {
+                char current = map[x, y];
+
+                if (current == OBSTACLE) { continue; }
+
+                map[x, y] = OBSTACLE;
+
+                if (!IsInfinite())
+                {
+                    map[x, y] = current;
+                    continue;
+                }
+
+                ++infinites;
+                map[x, y] = 'O';
+            }
+        }
+
+
+        return infinites.ToString();
+
+        bool IsInfinite()
+        {
+            Vector2Int position = origin;
+            Vector2Int direction = new(0, 1);
+            HashSet<(Vector2Int, Vector2Int)> path = [(position, direction)];
+            int n = 0;
+
+            while (n < 100000)
+            {
+                n++;
+                Vector2Int next = position + new Vector2Int(direction.x, -direction.y);
+
+                if (!IsPointInGrid(next)) { return false; }
+                else if (map[next.x, next.y] == OBSTACLE)
+                {
+                    direction.RotateDegrees(-90);
+                }
+                else
+                {
+                    if (!path.Add((next, direction)))
+                    {
+                        return true;
+                    }
+
+                    position = next;
+                }
+            }
+
+            throw new Exception("fail save has activated");
+        }
+    }
+
+    private bool IsPointInGrid(Vector2Int point)
+    {
+        if (point.x < 0 || point.y < 0) { return false; }
+
+        if (point.x >= map.GetLength(0) || point.y >= map.GetLength(1)) { return false; }
+
+        return true;
     }
 }
