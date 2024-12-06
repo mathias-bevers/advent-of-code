ade5996f8cc0849931f9455d39f681b024462a2f
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-06 09:34:51 +0100

    solved star one
    - solved star one, note had to invert the direction's y-axis.

--- src/days/2024/Day0624.cs
+++ src/days/2024/Day0624.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using advent_of_code.utils;
 
 namespace advent_of_code.days;
@@ -7,9 +8,12 @@ internal class Day0624 : IDay
     public DateTime date { get; } = new(2024, 12, 06);
 
     private const char START_POINT = '^';
+    private const char VISITED_POINT = 'X';
+    private const char OBSTACLE = '#';
 
     private char[,] map = new char[0, 0];
     private Vector2Int position = new();
+    private Vector2Int direction = new(0, 1);
 
     public void PopulateData(string raw)
     {
@@ -35,7 +39,44 @@ internal class Day0624 : IDay
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        int width = map.GetLength(0);
+        int height = map.GetLength(1);
+        int limit = width * height * 10;
+        int n = 0;
+
+        do
+        {
+            ++n;
+
+            map[position.x, position.y] = VISITED_POINT;
+
+            Vector2Int next = position + new Vector2Int(direction.x, -direction.y);
+
+            if (next.x < 0 || next.x >= width || next.y < 0 || next.y >= height) { break; }
+
+            if (map[next.x, next.y] != OBSTACLE)
+            {
+                position = next;
+                continue;
+            }
+
+            direction.RotateDegrees(-90);
+        }
+        while (n < limit);
+
+        int visited = 0;
+
+        for (int y = 0; y < height; ++y)
+        {
+            for (int x = 0; x < width; ++x)
+            {
+                if (map[x, y] != VISITED_POINT) { continue; }
+
+                ++visited;
+            }
+        }
+
+        return visited.ToString();
     }
 
     public string SolveStarTwo()
