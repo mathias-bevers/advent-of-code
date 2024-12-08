bd1ef265e25531402936cdfd1cc7a28eccb254d8
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-08 15:15:28 +0100

    solved star one
    - solved star one by finding all frequencies, and do a double loop to
    find their antinodes.
    - added all possible antinodes to hashset to prevent duplicates.

--- src/days/2024/Day0824.cs
+++ src/days/2024/Day0824.cs
@@ -26,11 +26,59 @@ internal class Day0824 : IDay
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        Dictionary<char, List<Vector2Int>> frequencies = [];
+
+        for (int y = 0; y < grid.GetLength(1); ++y)
+        {
+            for (int x = 0; x < grid.GetLength(0); ++x)
+            {
+                char current = grid[x, y];
+
+                if (current == EMPTY) { continue; }
+
+                if (!frequencies.TryAdd(current, new List<Vector2Int>() { new(x, y) }))
+                {
+                    frequencies[current].Add(new Vector2Int(x, y));
+                }
+            }
+        }
+
+        HashSet<Vector2Int> antinodes = [];
+
+        for (int i = 0; i < frequencies.Count; ++i)
+        {
+            List<Vector2Int> collection = frequencies.ElementAt(i).Value;
+
+            for (int ii = 0; ii < collection.Count; ++ii)
+            {
+                for (int iii = 0; iii < collection.Count; ++iii)
+                {
+                    if (iii == ii) continue;
+
+                    Vector2Int possibleLocation = (collection[iii] - collection[ii]) * -1;
+                    possibleLocation += collection[ii];
+
+                    if (!IsPointInGrid(possibleLocation)) { continue; }
+
+                    antinodes.Add(possibleLocation);
+                }
+            }
+        }
+
+        return antinodes.Count.ToString();
     }
 
     public string SolveStarTwo()
     {
         throw new NotImplementedException();
+    }
+
+    private bool IsPointInGrid(Vector2Int point)
+    {
+        if (point.x < 0 || point.x >= grid.GetLength(0)) { return false; }
+
+        if (point.y < 0 || point.y >= grid.GetLength(1)) { return false; }
+
+        return true;
     }
 }
