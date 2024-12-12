18bd1fcffd061fbea518a04304247f94df28421a
Mathias Bevers <mathiasbevers@outlook.com>
2024-12-12 10:21:21 +0100

    added and parsed example
    - added and parsed example to a two dimensional char array.

---
+++ src/days/2024/Day1224.cs
@@ -0,0 +1,32 @@
+
+namespace advent_of_code.days;
+
+internal class Day1224 : IDay
+{
+    public DateTime date { get; } = new DateTime(2024, 12, 12);
+    private char[,] grid = new char[0, 0];
+
+    public void PopulateData(string raw)
+    {
+        string[] rows = raw.Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
+        grid = new char[rows.Length, rows[0].Length];
+
+        for (int y = 0; y < grid.GetLength(1); ++y)
+        {
+            for (int x = 0; x < grid.GetLength(0); ++x)
+            {
+                grid[x,y] = rows[y][x];
+            }
+        }
+    }
+
+    public string SolveStarOne()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string SolveStarTwo()
+    {
+        throw new NotImplementedException();
+    }
+}
