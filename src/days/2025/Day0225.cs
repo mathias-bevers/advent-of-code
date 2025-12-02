088b202d571ed634c84f9b75c91d2f22971eb9da
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-02 08:49:44 +0100

    solved star one
    - solved star one by number lenght check then compare front and back as
    a string.
    - digid calculation from:
    https://stackoverflow.com/questions/4483886/how-can-i-get-a-count-of-the-total-number-of-digits-in-a-number.

--- src/days/2025/Day0225.cs
+++ src/days/2025/Day0225.cs
@@ -1,10 +1,12 @@
 namespace advent_of_code.days;
-using Range = (int min, int max);
+
+using advent_of_code.utils;
+using Range = (long min, long max);
 
 internal class Day0225 : IDay
 {
     public DateTime date { get; } = new(2025, 12, 02);
-    private Range[] ranges = Array.Empty<Range>(); 
+    private Range[] ranges = Array.Empty<Range>();
 
     public void PopulateData(string raw)
     {
@@ -14,8 +16,8 @@ internal class Day0225 : IDay
         for (int i = 0; i < ranges.Length; i++)
         {
             string[] parts = rawRanges[i].Split('-');
-            int min = int.Parse(parts[0]);
-            int max = int.Parse(parts[1]);
+            long min = long.Parse(parts[0]);
+            long max = long.Parse(parts[1]);
 
             ranges[i] = (min, max);
         }
@@ -23,7 +25,34 @@ internal class Day0225 : IDay
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        long sumInvalidIDs = 0;
+
+        for (int i = 0; i < ranges.Length; ++i)
+        {
+            for (long ii = ranges[i].min; ii <= ranges[i].max; ii++)
+            {
+                // check if the number has an even amount of digids.
+                if (Math.Floor(Math.Log10(ii) + 1) % 2 != 0)
+                {
+                    continue;
+                }
+
+                // split the string in two so compare.
+                string id = ii.ToString();
+                string begin = id[..(id.Length / 2)];
+                string end = id[(id.Length / 2)..];
+
+                if (!string.Equals(begin, end))
+                {
+                    continue;
+                }
+
+                sumInvalidIDs += ii;
+            }
+
+        }
+
+        return sumInvalidIDs.ToString();
     }
 
     public string SolveStarTwo()
