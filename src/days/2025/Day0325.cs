341531f0dfd8c9b78f553eb734fb32ef62c1480e
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-03 08:31:30 +0100

    solved star one
    - solved star one by looking up the higest index in the byte array - 1,
    then looking up the index searching from the index found.

--- src/days/2025/Day0325.cs
+++ src/days/2025/Day0325.cs
@@ -3,26 +3,57 @@ namespace advent_of_code.days;
 internal class Day0325 : IDay
 {
     public DateTime date { get; } = new(2025, 12, 03);
-    private byte[][] batteries = [];
+    private byte[][] batteryBanks = [];
 
     public void PopulateData(string raw)
     {
         string[] lines = raw.Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
-        batteries = new byte[lines.Length][];
+        batteryBanks = new byte[lines.Length][];
 
         for (int i = 0; i < lines.Length; i++)
         {
-            batteries[i] = new byte[lines[i].Length];
-            for (int ii = 0; ii < batteries[i].Length; ii++)
+            batteryBanks[i] = new byte[lines[i].Length];
+            for (int ii = 0; ii < batteryBanks[i].Length; ii++)
             {
-                batteries[i][ii] = (byte)Char.GetNumericValue(lines[i][ii]);
+                batteryBanks[i][ii] = (byte)Char.GetNumericValue(lines[i][ii]);
             }
         }
     }
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();        
+        int totalOutput = 0;
+
+        for (int i = 0; i < batteryBanks.Length; i++)
+        {
+            byte[] batteries = batteryBanks[i];
+            (byte value, int index) firstHighest = (0, -1);
+            for (int ii = 0; ii < batteries.Length - 1; ii++)
+            {
+                if (batteries[ii] <= firstHighest.value)
+                {
+                    continue;
+                }
+
+                firstHighest = (batteries[ii], ii);
+            }
+
+            byte secondHighest = 0;
+            for (int ii = firstHighest.index + 1; ii < batteries.Length; ii++)
+            {
+                if (batteries[ii] <= secondHighest)
+                {
+                    continue;
+                }
+
+                secondHighest = batteries[ii];
+            }
+
+            int batteryOutput = (firstHighest.value * 10) + secondHighest;
+            totalOutput += batteryOutput;
+        }
+
+        return totalOutput.ToString();
     }
 
     public string SolveStarTwo()
