88f51cbfea6e0041f4b3ebe71167009ab0c4305d
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-04 06:33:21 +0100

    solved star one
    - solved star one by looking through the roll grid and scanning
    neighbors of the roll.

--- src/days/2025/Day0425.cs
+++ src/days/2025/Day0425.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using advent_of_code.utils;
 
@@ -6,6 +7,8 @@ namespace advent_of_code.days;
 internal class Day0425 : IDay
 {
     public DateTime date { get; } = new(2025, 12, 04);
+
+    private const char ROLL = '@';
     private char[,] paperRolls = new char[0, 0];
 
     public void PopulateData(string raw)
@@ -24,11 +27,71 @@ internal class Day0425 : IDay
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        int accesibleCount = 0;
+
+        for (int y = 0; y < paperRolls.GetLength(1); y++)
+        {
+            for (int x = 0; x < paperRolls.GetLength(0); x++)
+            {
+                if (paperRolls[x, y] != ROLL)
+                {
+                    continue;
+                }
+
+                int neighborCount = GetNeighborCount(x, y);
+
+                if (neighborCount >= 4)
+                {
+                    continue;
+                }
+
+                ++accesibleCount;
+            }
+        }
+        
+        return accesibleCount.ToString();
     }
 
     public string SolveStarTwo()
     {
         throw new NotImplementedException();
+    }
+
+    private int GetNeighborCount(int x, int y)
+    {
+        int neighborCount = 0;
+
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int ii = -1; ii <= 1; ii++)
+            {
+                if (i == 0 && ii == 0)
+                {
+                    continue;
+                }
+
+                int nX = x + ii;
+                int nY = y + i;
+
+                if (nX < 0 || nY < 0)
+                {
+                    continue;
+                }
+
+                if(nX >= paperRolls.GetLength(0) || nY >= paperRolls.GetLength(1))
+                {
+                    continue;
+                }
+
+                if(paperRolls[nX, nY] != ROLL)
+                {
+                    continue;
+                }
+
+                ++neighborCount;
+            }
+        }
+
+        return neighborCount;
     }
 }
