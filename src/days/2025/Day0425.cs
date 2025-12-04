9847b7ae3aee138bbedf79e0184d1a4be5afd2e9
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-04 06:54:41 +0100

    solved star two
    - solved star one by moving the solution of star one into a recusivly
    callable method.

--- src/days/2025/Day0425.cs
+++ src/days/2025/Day0425.cs
@@ -9,6 +9,7 @@ internal class Day0425 : IDay
     public DateTime date { get; } = new(2025, 12, 04);
 
     private const char ROLL = '@';
+    private const char REMOVED_ROLL = 'x';
     private char[,] paperRolls = new char[0, 0];
 
     public void PopulateData(string raw)
@@ -27,37 +28,63 @@ internal class Day0425 : IDay
 
     public string SolveStarOne()
     {
+        char[,] grid = paperRolls.Clone() as char[,] ??
+            throw new NullReferenceException("cannot copy array.");
+        int accesibleCount = GetAccesibleRolls(1, ref grid);
+        return accesibleCount.ToString();
+    }
+
+    public string SolveStarTwo()
+    {
+        char[,] grid = paperRolls.Clone() as char[,] ??
+            throw new NullReferenceException("cannot copy array.");
+        int accesibleCount = GetAccesibleRolls(10000, ref grid);
+        return accesibleCount.ToString();
+    }
+
+    private int GetAccesibleRolls(int depth, ref char[,] grid)
+    {
+        if (depth == 0)
+        {
+            return 0;
+        }
+
         int accesibleCount = 0;
 
-        for (int y = 0; y < paperRolls.GetLength(1); y++)
+        List<(int x, int y)> removedRolls = [];
+
+        for (int y = 0; y < grid.GetLength(1); y++)
         {
-            for (int x = 0; x < paperRolls.GetLength(0); x++)
+            for (int x = 0; x < grid.GetLength(0); x++)
             {
-                if (paperRolls[x, y] != ROLL)
+                if (grid[x, y] != ROLL)
                 {
                     continue;
                 }
 
-                int neighborCount = GetNeighborCount(x, y);
+                int neighborCount = GetNeighborCount(x, y, ref grid);
 
                 if (neighborCount >= 4)
                 {
                     continue;
                 }
 
+                removedRolls.Add((x, y));
                 ++accesibleCount;
             }
         }
-        
-        return accesibleCount.ToString();
+
+        if (accesibleCount == 0) { return 0; }
+
+        for (int i = 0; i < removedRolls.Count; i++)
+        {
+            grid[removedRolls[i].x, removedRolls[i].y] = REMOVED_ROLL;
+        }
+
+        return accesibleCount + GetAccesibleRolls(depth - 1, ref grid);
     }
 
-    public string SolveStarTwo()
-    {
-        throw new NotImplementedException();
-    }
-
-    private int GetNeighborCount(int x, int y)
+    private int GetNeighborCount(int x, int y, ref char[,] grid)
     {
         int neighborCount = 0;
 
@@ -78,12 +105,12 @@ internal class Day0425 : IDay
                     continue;
                 }
 
-                if(nX >= paperRolls.GetLength(0) || nY >= paperRolls.GetLength(1))
+                if (nX >= grid.GetLength(0) || nY >= grid.GetLength(1))
                 {
                     continue;
                 }
 
-                if(paperRolls[nX, nY] != ROLL)
+                if (grid[nX, nY] != ROLL)
                 {
                     continue;
                 }
