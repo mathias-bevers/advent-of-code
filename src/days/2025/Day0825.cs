8f179eda3ffdf99f2759feea82537905a36674f3
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-08 15:22:37 +0100

    solved star one
    - solved star one by using two arrays where one acted as a map for the
    second.

--- src/days/2025/Day0825.cs
+++ src/days/2025/Day0825.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using advent_of_code.utils;
+using Connection = (int from, int to, double distance); // index, index, distance
 
 namespace advent_of_code.days;
 
@@ -7,12 +9,15 @@ internal class Day0825 : IDay
     public DateTime date { get; } = new(2025, 12, 08);
 
     private Vector3Int[] junctionBoxes = [];
+    private Connection[] connections = [];
+    private int pairsNeeded = -1;
 
     public void PopulateData(string raw)
     {
         string[] lines = raw.Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
         junctionBoxes = new Vector3Int[lines.Length];
 
+        // parse input to an vector 3 int array.
         for (int i = 0; i < junctionBoxes.Length; ++i)
         {
             string[] values = lines[i].Split(',', StringSplitOptions.RemoveEmptyEntries);
@@ -22,11 +27,77 @@ internal class Day0825 : IDay
 
             junctionBoxes[i] = new Vector3Int(x, y, z);
         }
+
+        // pre compute all the distances between the junctions.
+        List<Connection> tmp = [];
+        for (int i = 0; i < junctionBoxes.Length; ++i)
+        {
+            Vector3Int from = junctionBoxes[i];
+            for (int ii = i + 1; ii < junctionBoxes.Length; ++ii)
+            {
+                Vector3Int to = junctionBoxes[ii];
+                double distance = Vector3Int.Distance(from, to);
+
+                tmp.Add((i, ii, distance));
+            }
+        }
+
+        // order precomputed connection by distance.
+        connections = [.. tmp.OrderBy(c => c.distance)];
+
+        // set the needed pairs dynamiclly to based on the input's line count.
+        pairsNeeded = lines.Length == 20 ? 10 : 1000;
     }
 
     public string SolveStarOne()
     {
-        throw new NotImplementedException();
+        // create two arrays where circuits acts as a map for the sizes.
+        int[] circuits = new int[junctionBoxes.Length];
+        int[] circuitSizes = new int[junctionBoxes.Length];
+
+        // init the arrays with their defualt values.
+        for (int i = 0; i < junctionBoxes.Length; ++i)
+        {
+            circuits[i] = i;
+            circuitSizes[i] = 1;
+        }
+
+        // loop for the needed pairs.
+        for (int i = 0; i < pairsNeeded; ++i)
+        {
+            // get the map indexes for the current connection.
+            Connection connection = connections[i];
+            int a = circuits[connection.from];
+            int b = circuits[connection.to];
+
+            if (a == b)
+            {
+                continue;
+            }
+
+            // loop though all the junctions to find the to connection.
+            for (int ii = 0; ii < junctionBoxes.Length; ii++)
+            {
+                if (circuits[ii] != b)
+                {
+                    continue;
+                }
+
+                // point the to connection to the to connection.
+                circuits[ii] = a;
+            }
+
+            // add the from circuit size to the to circuit size and set to zero.  
+            circuitSizes[a] += circuitSizes[b];
+            circuitSizes[b] = 0;
+        }
+
+        // sort the array and reverse to get the sizes decending.
+        Array.Sort(circuitSizes);
+        Array.Reverse(circuitSizes);
+
+        long result = circuitSizes[0] * circuitSizes[1] * circuitSizes[2];
+        return result.ToString();
     }
 
     public string SolveStarTwo()
--- src/utils/Vector3.cs
+++ src/utils/Vector3.cs
@@ -5,6 +5,8 @@ namespace advent_of_code.utils;
 
 internal struct Vector3Int
 {
+    public static readonly Vector3Int positiveInfinity = new(int.MaxValue);
+
     public int x { get; set; }
     public int y { get; set; }
     public int z { get; set; }
@@ -18,11 +20,27 @@ internal struct Vector3Int
         this.z = z;
     }
 
+    public Vector3Int(int v)
+    {
+        x = v;
+        y = v;
+        z = v;
+    }
+
     public Vector3Int()
     {
         x = 0;
         y = 0;
         z = 0;
+    }
+
+    public static double Distance(Vector3Int a, Vector3Int b)
+    {
+        double x = a.x - b.x;
+        double y = a.y - b.y;
+        double z = a.z - b.z;
+
+        return Math.Sqrt(x * x + y * y + z * z);
     }
 
     public static Vector3Int operator +(Vector3Int a, Vector3Int b)
@@ -61,5 +79,18 @@ internal struct Vector3Int
         return x == other.x && y == other.y && z == other.z;
     }
 
-    public override readonly string ToString() => string.Concat('[', x, ", ", y, ", ", z, ']');
+    public override readonly string ToString()
+    {
+        System.Text.StringBuilder sb = new();
+
+        sb.Append('[');
+        sb.Append(x.ToString("00000"));
+        sb.Append(", ");
+        sb.Append(y.ToString("00000"));
+        sb.Append(", ");
+        sb.Append(z.ToString("00000"));
+        sb.Append(']');
+
+        return sb.ToString();
+    }
 }
