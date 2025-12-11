9b05bd2028e5c81750007e627939f78f99ade4fd
Mathias Bevers <mathiasbevers@protonmail.com>
2025-12-11 10:01:31 +0100

    star two works on example, puzzle is very slow
    - star two now keeps track of the path.
    - when out is reached the path is check wether it's valid.
    - works for example, puzzle is not done in 5 sec so trying to optimize.

--- src/days/2025/Day1125.cs
+++ src/days/2025/Day1125.cs
@@ -9,14 +9,23 @@ internal class Day1125 : IDay
 
     private const string YOU = "you";
     private const string OUT = "out";
+    private const string SVR = "svr";
+    private const string DAC = "dac";
+    private const string FFT = "fft";
 
 
     private Dictionary<string, string[]> devices = [];
+    private Dictionary<string, string[]> example = [];
+
+    bool isExample = false;
     // Dictionary<int, List<int>> devices = []; //TODO: test if faster.
 
     public void PopulateData(string raw)
     {
-        string[] lines = raw.Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
+        string[] chunks = raw.Split(Utils.NEW_CHUNKS, StringSplitOptions.RemoveEmptyEntries);
+        isExample = chunks.Length > 1;
+
+        string[] lines = chunks[0].Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
 
         for (int i = 0; i < lines.Length; i++)
         {
@@ -32,15 +41,36 @@ internal class Day1125 : IDay
 
             devices[key] = routes;
         }
+
+        if (!isExample)
+        {
+            return;
+        }
+
+        lines = chunks[1].Split(Utils.NEW_LINES, StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            // get the first three chars as the key, then start splitting from 5 to get the routes.
+            string device = lines[i];
+            string key = device[..3];
+            string[] routes = device[5..].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (example.ContainsKey(key))
+            {
+                Logger.Warning("overwriting value of key: " + key);
+            }
+
+            example[key] = routes;
+        }
     }
 
     public string SolveStarOne()
     {
-        Dictionary<string, long> preCalculated = new();
+        Dictionary<string, long> preCalc = new();
 
         try
         {
-            long numberOfPaths = FindPaths(YOU, ref preCalculated, 0);
+            long numberOfPaths = FindYouPaths(YOU, ref preCalc, 0);
             return numberOfPaths.ToString();
         }
         catch (FailSaveException e)
@@ -52,12 +82,23 @@ internal class Day1125 : IDay
 
     public string SolveStarTwo()
     {
-        throw new NotImplementedException();
+        Dictionary<string, long> preCalc = new();
+
+        try
+        {
+            long numberOfPaths = FindSvrPaths(SVR, ref preCalc, new List<string>(), 0);
+            return numberOfPaths.ToString();
+        }
+        catch (FailSaveException e)
+        {
+            Logger.Info(e.Message);
+            return "error";
+        }
     }
 
-    private long FindPaths(string key, ref Dictionary<string, long> preCalculated, int n)
+    private long FindYouPaths(string key, ref Dictionary<string, long> preCalc, int n)
     {
-        if (n == 1_000_000)
+        if (n == 50) // tested max depth in my case this is 22 so allow for wiggle room.
         {
             throw new FailSaveException($"triggered fail save on: " + key);
         }
@@ -67,7 +108,7 @@ internal class Day1125 : IDay
             return 1;
         }
 
-        if (preCalculated.TryGetValue(key, out long value))
+        if (preCalc.TryGetValue(key, out long value))
         {
             return value;
         }
@@ -76,9 +117,64 @@ internal class Day1125 : IDay
         string[] routes = devices[key];
         for (int i = 0; i < routes.Length; ++i)
         {
-            value += FindPaths(routes[i], ref preCalculated, ++n);
+            value += FindYouPaths(routes[i], ref preCalc, ++n);
         }
 
         return value;
+    }
+
+    private long FindSvrPaths(string key, ref Dictionary<string, long> preCalc,
+                              List<string> path, int n)
+    {
+        if (n == 10_000)
+        {
+            throw new FailSaveException($"triggered fail save on: " + key);
+        }
+
+        if (string.Equals(key, OUT))
+        {
+            return IsValidPath(ref path) ? 1 : 0;
+        }
+
+        if (preCalc.TryGetValue(key, out long value))
+        {
+            return value;
+        }
+
+        value = 0;
+        string[] routes = isExample ? example[key] : devices[key];
+        List<string> newPath = new(path);
+        newPath.Add(key);
+        for (int i = 0; i < routes.Length; ++i)
+        {
+            value += FindSvrPaths(routes[i], ref preCalc, newPath, ++n);
+        }
+
+        return value;
+    }
+
+    private bool IsValidPath(ref List<string> path)
+    {
+        bool foundFFT = false;
+        bool foundDAC = false;
+
+        for (int i = 0; i < path.Count; ++i)
+        {
+            if (path[i].Equals(FFT))
+            {
+                foundFFT = true;
+            }
+            else if (path[i].Equals(DAC))
+            {
+                foundDAC = true;
+            }
+
+            if (foundFFT && foundDAC)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
